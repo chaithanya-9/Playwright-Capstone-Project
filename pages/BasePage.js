51d3dc4ef9fd5e2df2cc6cb396c1c461fa01c2e5
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-22 11:23:46 +0530

    remove additiional spaces in code

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -1,12 +1,9 @@
 class BasePage {
-
     constructor(page) {
         this.page = page;
-
         // global locators
         this.searchBox = page.getByRole('textbox', { name: 'search' });
         this.searchButton = page.locator('#search').getByRole('button');
-
         // header locators
         this.myAccountDropdown = page.getByRole('link', { name: ' My Account' });
         this.registerLink = page.getByRole('link', { name: 'Register' });
@@ -14,34 +11,28 @@ class BasePage {
         this.shoppingCartLink = page.locator('a[title="Shopping Cart"]');
         this.checkoutLink = page.locator('a[title="Checkout"]');
     }
-
     // navigate to the base URL
     async navigate() {
         await this.page.goto('https://naveenautomationlabs.com/opencart/');
     }
-
     // search for product
     async searchForProduct(keyword) {
         await this.searchBox.fill(keyword);
         await this.searchButton.click();
     }
-
     // open My Account dropdown from header
     async clickMyAccount() {
         await this.myAccountDropdown.click();
     }
-
     // navigate directly to login page
     async navigateToLogin() {
         await this.clickMyAccount();
         await this.loginLink.click();
     }
-
     // navigate directly to register page
     async navigateToRegister() {
         await this.clickMyAccount();
         await this.registerLink.click();
     }
 }
-
 module.exports = { BasePage };
