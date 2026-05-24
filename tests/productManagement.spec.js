5b2ea87a34fede489799555c5b6748aceadb61c5
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-24 12:24:58 +0530

    add productManagement suite and TC-01 for valid search

---
+++ tests/productManagement.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@playwright/test');
+const { ProductPage } = require('../pages/ProductPage');
+
+test.describe('Product Management Service Tests', () => {
+
+    // test 01
+    test('Test-01: Verify searching for a valid existing product returns the correct results grid', async ({ page }) => {
+        // initialize page object
+        const productPage = new ProductPage(page);
+        // navigate to website
+        await productPage.navigate();
+        // search for a product that exists in database
+        await productPage.searchForProduct('MacBook');
+        // check if page redirected to search results page
+        await expect(page).toHaveURL(/.*route=product\/search/);
+        const resultCount = await productPage.productCards.count();
+        await expect(resultCount).toBeGreaterThan(0);
+        // verify first product card is visible
+        await expect(productPage.productCards.first()).toBeVisible();
+        // check only for first product card because we already checked resultCount is greater than 0 and checked for first card to be visible so we save time not checking for rest of the cards 
+    });
+
+})
