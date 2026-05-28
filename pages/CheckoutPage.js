24006bb22259ad39b043134a6494ac8a3006e4cb
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-28 07:05:19 +0530

    add address selection, confirm order, success page and price verification locators

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -26,13 +26,34 @@ class CheckoutPage extends BasePage {
         this.shippingMethodContinueButton = page.locator('#button-shipping-method');
         this.newDeliveryAddressRadioButton = page.locator('input[name="shipping_address"][value="new"]');
         this.flatShippingRateRadio = page.locator('input[value="flat.flat"]');
+        this.deliveryMethodCommentBox = page.locator('textarea[name="comment"]');
+        // address selection locators
+        this.newDeliveryAddressRadioButton = page.locator('input[value="new"]');
+        // address form locators for new address
+        this.newAddressFirstName = page.locator('#input-shipping-firstname');
+        this.newAddressLastName = page.locator('#input-shipping-lastname');
+        this.newAddress1 = page.locator('#input-shipping-address-1');
+        this.newAddressCity = page.locator('#input-shipping-city');
+        this.newAddressPostcode = page.locator('#input-shipping-postcode');
+        this.newAddressCountry = page.locator('#input-shipping-country');
+        this.newAddressZone = page.locator('#input-shipping-zone');
         // payment locatos
         this.termsCheckbox = page.getByRole('checkbox');
         this.paymentMethodContinueButton = page.locator('#button-payment-method');
         this.confirmOrderButton = page.locator('#button-confirm');
         this.successHeading = page.getByRole('heading', { name: 'Your order has been placed!' });
+        this.codRadioButton = page.locator('input[value="cod"]');
+        // confirm order locators
+        this.confirmProductName = page.locator('#collapse-checkout-confirm tbody tr td.text-left a');
+        // success page locators
+        this.successContinueButton = page.locator('a.btn.btn-primary:text("Continue")');
+        this.orderSuccessMessage = page.locator('#content p:nth-child(2)');
         // validation locators
         this.inputFieldWarning = page.locator('.text-danger');
+        this.warningBanner = page.locator('.alert-danger');
+        // price verification locators
+        this.confirmOrderTable = page.locator('#collapse-checkout-confirm');
+        this.finalTotalText = page.locator('#collapse-checkout-confirm tfoot tr:last-child td:nth-child(2)');
     }
     // action methods
     // navigate directly to Checkout Page
