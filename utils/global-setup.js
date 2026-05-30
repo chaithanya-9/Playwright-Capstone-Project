61ad277fa75458ec86207cf3d605937ee0033d4e
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-30 21:40:13 +0530

    modify .auth path and register page locators

--- utils/global-setup.js
+++ utils/global-setup.js
@@ -18,20 +18,20 @@ async function globalSetup() {
     await page.getByRole('textbox', { name: 'Last Name' }).fill('User');
     await page.getByRole('textbox', { name: 'E-Mail' }).fill(uniqueEmail);
     await page.getByRole('textbox', { name: 'Telephone' }).fill(uniquePhone);
-    await page.getByRole('textbox', { name: 'Password', exact: true }).fill(password);
-    await page.getByRole('textbox', { name: 'Password Confirm' }).fill(password);
+    await page.getByRole('textbox', { name: '* Password', exact: true }).fill(password);
+    await page.getByRole('textbox', { name: '* Password Confirm' }).fill(password);
     // agree to privacy policy and submit
     await page.getByRole('checkbox').check();
     await page.getByRole('button', { name: 'Continue' }).click();
     // wait for the success message to ensure registration is complete
     await page.waitForURL(/.*route=account\/success/);
     // create the .auth directory if it doesn't exist
-    const authDirPath = path.join(__dirname, '../playwright/.auth');
+    const authDirPath = path.join(__dirname, '../.auth');
     if (!fs.existsSync(authDirPath)) {
         fs.mkdirSync(authDirPath, { recursive: true });
     }
     // save the storage state to a file for later use
-    await page.context().storageState({ path: 'playwright/.auth/user.json' });
+    await page.context().storageState({ path: '.auth/user.json' });
     // close the browser
     await browser.close();
 }
