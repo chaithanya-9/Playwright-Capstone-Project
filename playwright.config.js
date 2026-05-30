9098640971dc0e7d846f7ae51bfcd5baecc8f727
chaithanya-9 <chaithanyayadav09@gmail.com>
2026-05-31 00:08:52 +0530

    add authentication and modify configuration and ignore files

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
     testDir: './tests',
+    globalSetup: require.resolve('./utils/global-setup'),
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
